8831f0b2f816b82d966cf042612023724d8165be
hlabuschagne <hendrik.labuschagne@mirasecurity.com>
2024-10-03 10:35:57 +0200

    Add support for nmcli in cloud_init
    - Now use nmcli for the network management
    - Added optional argument nameserver

--- cloud_init_scripts/cloud_init_utils.py
+++ cloud_init_scripts/cloud_init_utils.py
@@ -1,9 +1,22 @@
 import argparse
 import os
 import subprocess
+from ipaddress import IPv4Network
 
 
-def generate_cloud_init_files(vm_username, vm_hashed_password, cloud_init_user_data_directory, cloud_init_meta_data_directory, vm_hostname, vm_static_ip_address=None, vm_ip_gateway=None, vm_ip_netmask=None, vm_dns_server_1=None, vm_dns_server_2=None):
+def generate_cloud_init_files(
+        vm_username,
+        vm_hashed_password,
+        cloud_init_user_data_directory,
+        cloud_init_meta_data_directory,
+        vm_hostname,
+        vm_static_ip_address=None,
+        vm_ip_gateway=None,
+        vm_ip_netmask=None,
+        vm_dns_server_1=None,
+        vm_dns_server_2=None,
+        vm_nameserver=None,
+    ):
     """
     Generate cloud-init scripts for configuring a virtual machine.
 
@@ -25,10 +38,14 @@ def generate_cloud_init_files(vm_username, vm_hashed_password, cloud_init_user_d
         The IP gateway for the virtual machine (default is None).
     vm_ip_netmask : str, optional
         The IP netmask for the virtual machine (default is None).
+        Supports dotted-decimal (eg. '255.255.255.255') and CIDR (eg. '/32') notations.
     vm_dns_server_1 : str, optional
         The primary DNS server for the virtual machine (default is None).
     vm_dns_server_2 : str, optional
         The secondary DNS server for the virtual machine (default is None).
+    vm_nameserver: str, optional
+        The nameserver for the virtual machine (default is None).
+        Multiple can be specified comma-seperated.
 
     Returns
     -------
@@ -41,6 +58,16 @@ def generate_cloud_init_files(vm_username, vm_hashed_password, cloud_init_user_d
     settings. The generated user data template is written to the specified `cloud_init_user_data_directory` file,
     while the metadata template containing only the hostname is written to the `cloud_init_meta_data_directory` file.
     """
+    # Convert netmask to desired notation.  dotted-decimal for ifcfg and CIDR for nmcli
+    if vm_ip_netmask is not None:
+        netmask_len = len(vm_ip_netmask.split('.'))
+        if netmask_len == 1 and len(vm_ip_netmask.split('/')) == 1:
+            vm_ip_netmask = f"/{vm_ip_netmask}"
+        if netmask_len == 4:
+            vm_ip_netmask = "/" + str(IPv4Network(f'0.0.0.0/{vm_ip_netmask}').prefixlen)
+    else:
+        vm_ip_netmask = ""
+
     cloud_init_user_data_template = '''#cloud-config
 user: {}
 password: "{}"
@@ -51,27 +78,30 @@ runcmd:
 '''.format(vm_username, vm_hashed_password, vm_hostname)
 
     if vm_static_ip_address:
-        cloud_init_user_data_template += "- set -x; sed -i -e 's/BOOTPROTO=\"dhcp\"/BOOTPROTO=\"static\"/g' /etc/sysconfig/network-scripts/ifcfg-eth0\n"
-        cloud_init_user_data_template += f"- set -x; echo 'IPADDR={vm_static_ip_address}' >> /etc/sysconfig/network-scripts/ifcfg-eth0\n"
+        cloud_init_user_data_template += f"- set -x; nmcli connection modify eth0 ipv4.method manual ipv4.addresses '{vm_static_ip_address}{vm_ip_netmask}'\n"
 
     if vm_ip_gateway:
-        cloud_init_user_data_template += f"- set -x; echo 'GATEWAY={vm_ip_gateway}' >> /etc/sysconfig/network-scripts/ifcfg-eth0\n"
-
-    if vm_ip_netmask:
-        cloud_init_user_data_template += f"- set -x; echo 'NETMASK={vm_ip_netmask}' >> /etc/sysconfig/network-scripts/ifcfg-eth0\n"
+        cloud_init_user_data_template += f"- set -x; nmcli connection modify eth0 ipv4.gateway '{vm_ip_gateway}'\n"
 
     if vm_dns_server_1:
         cloud_init_user_data_template += (
-            f"- set -x; if ! grep -q '^DNS1=' /etc/sysconfig/network-scripts/ifcfg-eth0; then echo 'DNS1={vm_dns_server_1}' >> /etc/sysconfig/network-scripts/ifcfg-eth0; else sed -i 's/^DNS1=.*/DNS1={vm_dns_server_1}/' /etc/sysconfig/network-scripts/ifcfg-eth0; fi\n"
+            f"- set -x; nmcli connection modify eth0 ipv4.dns '{vm_dns_server_1}'\n"
         )
 
     if vm_dns_server_2:
         cloud_init_user_data_template += (
-            f"- set -x; if ! grep -q '^DNS2=' /etc/sysconfig/network-scripts/ifcfg-eth0; then echo 'DNS2={vm_dns_server_2}' >> /etc/sysconfig/network-scripts/ifcfg-eth0; else sed -i 's/^DNS2=.*/DNS2={vm_dns_server_2}/' /etc/sysconfig/network-scripts/ifcfg-eth0; fi\n"
+            f"- set -x; nmcli connection modify eth0 +ipv4.dns '{vm_dns_server_2}'\n"
         )
 
+    if vm_nameserver:
+            cloud_init_user_data_template += (
+                f"- set -x; nmcli connection modify eth0 ipv4.dns-search '{vm_nameserver}'\n"
+            )
+
     # Restart the network to apply the changes
-    cloud_init_user_data_template += '- set -x; systemctl restart network\n'
+    cloud_init_user_data_template += (
+        f"- set -x; if uname -a | grep el7; then systemctl restart network; else systemctl restart NetworkManager ; fi\n"
+    )
 
     # Write the cloud_init_user_data_template to the specified output file
     with open(cloud_init_user_data_directory, 'w') as output_file:
@@ -147,13 +177,16 @@ def setup_arg_parser():
         '--vm_ip_gateway', help='IP gateway for the virtual machine',
     )
     parser_generate_cloud_init_iso.add_argument(
-        '--vm_ip_netmask', help='IP netmask for the virtual machine',
+        '--vm_ip_netmask', help="IP netmask for the virtual machine. Supports dotted-decimal (eg. '255.255.255.255') and CIDR (eg. '/32') notations",
     )
     parser_generate_cloud_init_iso.add_argument(
         '--vm_dns_server_1', help='Primary DNS server for the virtual machine',
     )
     parser_generate_cloud_init_iso.add_argument(
         '--vm_dns_server_2', help='Secondary DNS server for the virtual machine',
+    )
+    parser_generate_cloud_init_iso.add_argument(
+        '--vm_nameserver', help='Nameserver for the the virtual machine. Multiple can be specified comma-seperated.',
     )
     parser_generate_cloud_init_iso.add_argument(
         '--cloud_init_iso_name', required=True, help='Name of the cloud-init ISO image to generate',
@@ -181,7 +214,7 @@ if __name__ == '__main__':
             meta_data_directory,
             args.vm_hostname,
             args.vm_static_ip_address, args.vm_ip_gateway, args.vm_ip_netmask,
-            args.vm_dns_server_1, args.vm_dns_server_2,
+            args.vm_dns_server_1, args.vm_dns_server_2, args.vm_nameserver,
         )
         generate_cloud_init_iso(
             args.cloud_init_iso_name, user_data_directory, meta_data_directory,
